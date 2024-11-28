4c886e0d49b7622db12d0437f4e0a532a6e58de7
dheeraj pandey <dheeraj.systango@gmail.com>
2024-11-28 13:29:09 +0530

    edit balance transfer feature added

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from operator import index
 from flask_bcrypt import Bcrypt
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime, \
-    func, Boolean
+    func, Boolean, orm
 import datetime
 from flask import jsonify
 from sqlalchemy.exc import IntegrityError
@@ -78,6 +78,45 @@ class Bank_Account(AbstractModel):
         if deposite_amount <=0:
             raise ValueError('Deposite amount must be postive')
         self.account_balance += deposite_amount
+
+    def balance_transfer(self, from_user_id, to_user_id, amount):
+        if amount <= 0:
+            raise ValueError("Transfer amount must be greater than zero")
+
+        from_account = db.query(Bank_Account).filter(Bank_Account.user_id == from_user_id).first()
+        to_account = db.query(Bank_Account).filter(Bank_Account.user_id == to_user_id).first()
+        from_user = db.query(User).filter(User.id==from_user_id).first()
+        to_user = db.query(User).filter(User.id==to_user_id).first()
+
+        if not from_account or not to_account:
+            raise ValueError("One or both users do not have a bank account")
+
+        if from_account.account_balance < amount:
+            raise ValueError("Insufficient balance for transfer")
+
+        try:
+            from_account.withdrawal(amount)
+            # Create a notification
+            notification = Notifications(
+                content=f"- Amount {amount} and transferred to {to_user.username}.",
+                user_id=from_user_id
+            )
+            db.add(notification)
+            to_account.add_balance(amount)
+            notification = Notifications(
+                content=f"- Amount {amount} Deposited from {from_user.username}.",
+                user_id=to_user_id
+            )
+            db.add(notification)
+            db.commit()
+            return {
+                "from_user_balance": from_account.balance,
+                "to_user_balance": to_account.balance,
+            }
+        except Exception as e:
+            db.rollback()
+            raise ValueError(f"Transfer failed: {str(e)}")
+
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
